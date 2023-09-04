5dcf8644ea74a096f13f680f5623865541e65541
MarkRywell <gopro855@gmail.com>
2023-09-04 11:50:15 +0800

    Add basic OOP in Js

---
+++ Javascript/OOP/basicOOP.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }   
+}
+
+const person1 = new Person('Mark Gaje', 22);
+
+console.log(person1);
