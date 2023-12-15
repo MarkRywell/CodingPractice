78f4cfc1165bbbdbbffee09217e1e492efab29d0
MarkRywell <gopro855@gmail.com>
2023-12-15 17:12:32 +0800

    Ongoing problem

---
+++ Javascript/Problems/stocks.js
@@ -0,0 +1,13 @@
+// Coding Interview Question #2
+
+// Given a list of array, create a function that buys stocks at the lowest
+// value and sell in its highest and return the profit
+
+const stocks = [7, 1, 5, 3, 6, 4];
+
+function solution(stocks) {
+ 
+}
+
+console.log(solution(stocks))
+
