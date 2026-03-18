f552fd2f714dd2e113b405982a54b47f45317e87
MarkRywell <gopro855@gmail.com>
2026-03-18 12:13:26 +0800

    refactor: add ts for solid principle implementation

---
+++ Javascript/fundamentals/solid.ts
@@ -0,0 +1,42 @@
+class NotificationService {
+    send(message: string) {}
+}
+
+class MessageFormatter {
+    static formatMessage(message: string) {
+        return `[ALERT]: ${message}`;
+    }
+}
+
+class EmailNotification extends NotificationService {
+    send( message: string) {
+        console.log(`Sending Email: ${message}`);
+    }
+}
+
+class SMSNotification extends NotificationService {
+    send( message: string) {
+        console.log(`Sending SMS: ${message}`);
+    }
+}
+
+
+class NotificationSender {
+    service: any;
+
+    constructor(service: any) {
+        this.service = service;
+    }
+
+    apply(message: string) {
+        const formattedMessage = MessageFormatter.formatMessage(message);
+        return this.service.send(formattedMessage)
+    }
+}
+
+
+const emailNotification = new EmailNotification()
+const smsNotification = new SMSNotification()
+const notification = new NotificationSender(smsNotification)
+
+notification.apply("This is a test message for SMS notification.");
